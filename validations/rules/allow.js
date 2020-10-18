54c0b24616ad3b3536ea8824ba4c55e83c7aca27
Icaruk <adri-rk@hotmail.com>
2020-10-18 12:24:41 +0200

    unhandled promise fix

--- validations/rules/allow.js
+++ validations/rules/allow.js
@@ -1,76 +1,85 @@
 
 module.exports = (stringParaValidar, strAllow, config) => {
 	
-	if (! stringParaValidar) stringParaValidar = "";
-	let arrWl = strAllow.split(" "); // "a A 1" → ["a", "A", "1"]
-	
-	
-	let rest = stringParaValidar // para el modo objeto
-	let isRegex;
-	
-	
-	
-	// Itero el array, por ejemplo: ["a", "1", "_"]
-	for (_symbol of arrWl) {
+	try {
 		
-		// Obtengo la función/regex
-		let fnc = config.symbols[_symbol].regex;
+		if (! stringParaValidar) stringParaValidar = "";
+		let arrWl = strAllow.split(" "); // "a A 1" → ["a", "A", "1"]
 		
 		
-		// Si no encuentro, paso al siguiente símbolo
-		if (!fnc) continue;
+		let rest = stringParaValidar // para el modo objeto
+		let isRegex;
 		
 		
 		
-		// Es regex
-		if (typeof fnc === "object") {
+		// Itero el array, por ejemplo: ["a", "1", "_"]
+		for (_symbol of arrWl) {
 			
-			isRegex = true;
-			let regex = new RegExp(fnc);
-			rest = rest.replace(regex, ""); // quito todo lo que concuerde con el regex
+			// Obtengo la función/regex
+			let fnc = config.symbols[_symbol].regex;
 			
-		// Es función
-		} else if (typeof fnc === "function") {
 			
-			let error = fnc(stringParaValidar, config);
-			return error;
+			// Si no encuentro, paso al siguiente símbolo
+			if (!fnc) continue;
+			
+			
+			
+			// Es regex
+			if (typeof fnc === "object") {
+				
+				isRegex = true;
+				let regex = new RegExp(fnc);
+				rest = rest.replace(regex, ""); // quito todo lo que concuerde con el regex
+				
+			// Es función
+			} else if (typeof fnc === "function") {
+				
+				let error = fnc(stringParaValidar, config);
+				return error;
+				
+			};
 			
 		};
 		
-	};
-	
-	
-	
-	if (isRegex) {
 		
-		if (rest !== "") { // Si todavía queda algo, es que hay algo mal
+		
+		if (isRegex) {
 			
-			let error = config.rules.allow.messages.itsOnlyAllowed[config.language];
-			let maxIdx = arrWl.length - 1;
-			
-			
-			let strAnd = config.rules.allow.messages.and[config.language];
-			
-			
-			arrWl.forEach( (_x, idx) => {
+			if (rest !== "") { // Si todavía queda algo, es que hay algo mal
 				
-				if (idx === maxIdx && maxIdx > 0) error += strAnd; // último separador: "y" en lugar de ","
+				let error = config.rules.allow.messages.itsOnlyAllowed[config.language];
+				let maxIdx = arrWl.length - 1;
 				
-				error += config.symbols[_x].messages.name[config.language];
 				
-				if (idx < maxIdx - 1) error += ", "; // separador
-				if (idx === maxIdx) error += "."; // punto final
+				let strAnd = config.rules.allow.messages.and[config.language];
 				
-			});
-			
-			
-			return error;
-			
+				
+				arrWl.forEach( (_x, idx) => {
+					
+					if (idx === maxIdx && maxIdx > 0) error += strAnd; // último separador: "y" en lugar de ","
+					
+					error += config.symbols[_x].messages.name[config.language];
+					
+					if (idx < maxIdx - 1) error += ", "; // separador
+					if (idx === maxIdx) error += "."; // punto final
+					
+				});
+				
+				
+				return error;
+				
+			};
 		};
+		
+		
+		return "";
+		
+	} catch (err) {
+		
+		console.error( "validame error: ", err );
+		return "";
+		
 	};
-	
-	
-	return "";
 	
 };
 
