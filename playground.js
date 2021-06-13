44c40e25ad8ad547a05744da3bd04d127c4e6f1e
Icaruk <adri-rk@hotmail.com>
2021-06-13 23:35:07 +0200

    string method funcionando

--- playground.js
+++ playground.js
@@ -6,19 +6,27 @@ validameConfig.language = "es";
 
 
 
-let {isCorrect, message} = validame("asd 123 =_", {
-	none: [
-		/pepe/
-	],
-	all: [
-		/[a-z]/,
-		/[0-9]/,
-	],
-	any: [
-		/_/,
-		/=/,
-	]
+// let {isCorrect, message} = validame("asd 123 =_", {
+// 	none: [
+// 		/pepe/
+// 	],
+// 	all: [
+// 		/[a-z]/,
+// 		/[0-9]/,
+// 	],
+// 	any: [
+// 		/_/,
+// 		/=/,
+// 	]
 	
+// });
+
+
+
+let {isCorrect, message} = validame("123 asd ñ", {
+	all: [
+		"a", "1", "ñ"
+	],
 });
 
 
--- utils/executeValidation.js
+++ utils/executeValidation.js
@@ -1,28 +1,42 @@
 
-module.exports = function executeValidation(stringToValidate, validation, config) {
+module.exports = function executeValidation(stringToValidate, validationId, config) {
 	
 	const validations = config.validations;
 	
 	
-	if (typeof validation === "string") {
+	if (typeof validationId === "string") {
 		
 		console.log( "		------ STRING" );
-		// config.validations[validation];
-	
-	} else if (validation instanceof RegExp) {	
 		
-		const correct = validation.test(stringToValidate);
+		const validation = validations[validationId];
+		const regex = validation.regex;
+		
+		
+		if (regex.test(stringToValidate)) {
+			return {
+				isCorrect: true,
+			};
+		} else {
+			return {
+				isCorrect: false,
+				message: "No cumple regex",
+			};
+		};
+		
+	} else if (validationId instanceof RegExp) {	
+		
+		const correct = validationId.test(stringToValidate);
 		const objCorrect = {isCorrect: correct};
 		
 		if (!correct) objCorrect.message = "El texto no es válido";
 		
 		return objCorrect;
 		
-	} else if (typeof validation === "object" && validation !== null) {
+	} else if (typeof validationId === "object" && validationId !== null) {
 		
 		console.log( "		------ OBJECT" );
 		
-		for (_key in config.validations) {
+		for (_key in validations) {
 			
 			// Compruebo si es una validación predefinida (string u objeto)
 			
