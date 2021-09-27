e26ce3f2f4aca3be0111607b4c1e3c252d4aa8b5
Icaruk <adri-rk@hotmail.com>
2021-09-27 13:29:31 +0200

    Fix email validation versus possible ReDoS

--- tests/email.test.js
+++ tests/email.test.js
@@ -5,24 +5,34 @@ const {validame, validameConfig, validameUtils} = require("../index");
 describe("email", () => {
 	
 	[
-		["email 1", "asd@asd.es"],
-		["email 2", "1asd@asd.com"],
-		["email 3", "asd2@asd.jp"],
-		["email 4", "asd_asd@asd.com.es"],
-		["email 5", "asd-asd@asd.es"],
-		["email 6", "asd-@asd.es"],
-		["email 7", "asd_@asd.es"],
-		["email 8", "asd--@asd.es"],
-		["email 9", "asd__@asd.es"],
-		["email 10", "as-as@asd.es"],
-		["email 11", "as_as@asd.es"],
-		["email 12", "as--as--@asd.es"],
-		["email 13", "as__as__@asd.es"],
-	].forEach( _x => {
+		"asd@asd.es",
+		"1asd@asd.com",
+		"asd2@asd.jp",
+		"asd_asd@asd.com.es",
+		"asd-asd@asd.es",
+		"asd-@asd.es",
+		"asd_@asd.es",
+		"asd--@asd.es",
+		"asd__@asd.es",
+		"as-as@asd.es",
+		"as_as@asd.es", // 10
+		"as--as--@asd.es",
+		"as__as__@asd.es",
+		"test@asd.com",
+		"test-100@asd.com",
+		"test.100@asd.com",
+		"test111@test.com",
+		"test-100@test.net",
+		"test.100@test.com.au",
+		"test@1.com",
+		"test@gmail.com.com", // 20
+		"test+100@gmail.com",
+		"_asd@asd.es",
+	].forEach( (_x, _idx) => {
 		
-		test(_x[0], () => {
+		test(`email_${_idx}`, () => {
 			expect(
-				validame(_x[1], {
+				validame(_x, {
 					allow: "email"
 				})
 			).toBe("");
@@ -33,23 +43,26 @@ describe("email", () => {
 	
 	
 	[
-		["email fail 1", "asdasd.es"],
-		["email fail 2", "1asd@@asd.com"],
-		["email fail 3", "asd2@asd..jp"],
-		["email fail 4", "asd,asd@asd.com.es"],
-		["email fail 5", "asd!asd@asd.es"],
-		["email fail 6", "asd!asd@asd.abcd"],
+		"asdasd.es",
+		"1asd@@asd.com",
+		"asd2@asd..jp",
+		"asd,asd@asd.com.es",
+		"asd!asd@asd.es",
+		"asd!asd@asd.abcd",
+		"asd;asd@asd.abcd",
+		"asdçasd@asd.abcd",
+		"asdáasd@asd.abcd",
+		"asd asd@asd.abcd",
+		"asd	asd@asd.abcd", // 10
+		".asd@asd.es",
+		",asd@asd.es",
+		" asd@asd.es",
+		"-asd@asd.es",
+	].forEach( (_x, _idx) => {
 		
-		["email fail 6", "asd;asd@asd.abcd"],
-		["email fail 7", "asdçasd@asd.abcd"],
-		["email fail 8", "asdáasd@asd.abcd"],
-		["email fail 9", "asd asd@asd.abcd"],
-		["email fail 10", "asd	asd@asd.abcd"],
-	].forEach( _x => {
-		
-		test(_x[0], () => {
+				test(`email_${_idx}`, () => {
 			expect(
-				validame(_x[1], {
+				validame(_x, {
 					allow: "email"
 				})
 			).not.toBe("");
--- validations/symbols/email.js
+++ validations/symbols/email.js
@@ -5,15 +5,21 @@ exports.email = (stringParaValidar, config) => {
 	// Compruebo estructura
 	// *******************
 	
-	let regex = new RegExp(/^\w+([\.-]*\w+[\.-]*)*@\w+([\.-]?\w+)*(\.\w{2,16})+$/, "g");
-	let correcto = regex.test(stringParaValidar);
-	if (! correcto) return config.symbols.email.messages.invalid[config.language];
-	
+	// Compruebo que no empiece por un caracter inválido
+	let regex = new RegExp(/^[^_a-zA-Z0-9\s]/, "g");
+	let empiezaMal = regex.test(stringParaValidar);
+	if (empiezaMal) return config.symbols.email.messages.invalid[config.language] + "1";
 	
 	let strPreArroba = stringParaValidar.split("@")[0];
 	regex = new RegExp(/\(\)<>@,;:"\[\]ç%&áéíóúàèìòùäëÏöüâêîôû\s/gim);
 	let tieneCaracteres = regex.test(strPreArroba);
-	if (tieneCaracteres) return config.symbols.email.messages.invalid[config.language];
+	if (tieneCaracteres) return config.symbols.email.messages.invalid[config.language] + "2";
+	
+	// let regex = new RegExp(/^\w+([\.-]*\w+[\.-]*)*@\w+([\.-]?\w+)*(\.\w{2,16})+$/, "g"); // old: vulnerable to ReDoS
+	regex = new RegExp(/^[\w\.\-\+]+@\w+(\.[\w\-]{2,16})+$/, "g");
+	correcto = regex.test(stringParaValidar);
+	if (! correcto) return config.symbols.email.messages.invalid[config.language];
+	
 	
 	
 	return "";
