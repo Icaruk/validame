78cf566bf50f81210acea15fdf867f919488841b
Icaruk <adri-rk@hotmail.com>
2021-09-27 15:34:23 +0200

    wrong console logs

--- validations/symbols/email.js
+++ validations/symbols/email.js
@@ -8,12 +8,12 @@ exports.email = (stringParaValidar, config) => {
 	// Compruebo que no empiece por un caracter inválido
 	let regex = new RegExp(/^[^_a-zA-Z0-9\s]/, "g");
 	let empiezaMal = regex.test(stringParaValidar);
-	if (empiezaMal) return config.symbols.email.messages.invalid[config.language] + "1";
+	if (empiezaMal) return config.symbols.email.messages.invalid[config.language];
 	
 	let strPreArroba = stringParaValidar.split("@")[0];
 	regex = new RegExp(/\(\)<>@,;:"\[\]ç%&áéíóúàèìòùäëÏöüâêîôû\s/gim);
 	let tieneCaracteres = regex.test(strPreArroba);
-	if (tieneCaracteres) return config.symbols.email.messages.invalid[config.language] + "2";
+	if (tieneCaracteres) return config.symbols.email.messages.invalid[config.language];
 	
 	// let regex = new RegExp(/^\w+([\.-]*\w+[\.-]*)*@\w+([\.-]?\w+)*(\.\w{2,16})+$/, "g"); // old: vulnerable to ReDoS
 	regex = new RegExp(/^[\w\.\-\+]+@\w+(\.[\w\-]{2,16})+$/, "g");
